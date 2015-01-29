e7982090f41c16b50bfa1fa8e63358c5444fe087
Jerome Eteve <jerome@broadbean.com>
2015-01-29 15:59:35 +0000

    Bumped version to 0.04

--- lib/Alien/ImageMagick.pm
+++ lib/Alien/ImageMagick.pm
@@ -12,11 +12,11 @@ Alien::ImageMagick - cpanm compatible Image::Magick packaging.
 
 =head1 VERSION
 
-Version 0.03
+Version 0.04
 
 =cut
 
-our $VERSION = '0.03';
+our $VERSION = '0.04';
 
 =head1 PREREQS
 
